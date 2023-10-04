f449cd9a4284cf5b75ad1d97e314309a11bdd4f0
Vinicius Garcia <advviniciusgarcia@gmail.com>
2023-10-04 17:16:58 -0300

    First Commit

--- src/data-source.ts
+++ src/data-source.ts
@@ -8,6 +8,7 @@ const dataSourceConfig = (): DataSourceOptions => {
   const migrationPath: string = path.join(__dirname, "./migrations/**.{ts,js}");
 
   const dbUrl: string | undefined = process.env.DATABASE_URL + "?sslmode=require";
+  // const dbUrl: string | undefined = process.env.DATABASE_URL;
 
   if (!dbUrl) {
     throw new Error("Missing env var: 'DATABASE_URL'");
